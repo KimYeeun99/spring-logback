6a708f03e8c8447d3bb8345f2c1de81b7274f77e
KimYeeun99 <kyz0608@naver.com>
2024-02-07 22:10:58 +0900

    logback 설정

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # spring-logback
+
 - Spring Boot의 Logback 설정 예제입니다.
 - 정리한 Velog URL: https://velog.io/@yeeunn99/Spring-Boot-Log4j-%EC%84%A4%EC%A0%95
---
+++ src/test/java/com/logback/spring/SpringLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logback.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
